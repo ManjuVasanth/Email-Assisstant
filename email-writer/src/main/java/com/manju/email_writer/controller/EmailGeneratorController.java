49dedf3d5b8e1b0054fd24a3feb28277d868e20b
ManjuVasanth <manjuvasanthbe@gmail.com>
2025-04-28 19:08:47 -0500

    Email-writer working as expected

---
+++ email-writer/src/main/java/com/manju/email_writer/controller/EmailGeneratorController.java
@@ -0,0 +1,26 @@
+package com.manju.email_writer.controller;
+
+import com.manju.email_writer.model.EmailRequest;
+import com.manju.email_writer.service.EmailGeneratorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/key")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
